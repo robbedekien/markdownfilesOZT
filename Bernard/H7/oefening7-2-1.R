b1b540abeccbc54f7a9b8371fc58a63cbaea605e
NoCountryForKids <bernard.deploige@student.hogent.be>
2019-06-04 18:10:13 +0200

    H7-oefeningen

---
+++ Bernard/H7/oefening7-2-1.R
@@ -0,0 +1,50 @@
+# Clearen: plot, console, environment
+rm(list=ls())
+cat("\014")
+dev.off()
+library(lsr)
+
+# ========================= Chi-kwadraat - kruistabel (met inlezen van dataset) ============================
+
+# --------------------------------------------- Input Gegevens ---------------------------------------------
+
+data = survey
+data1 = data$Exer #Vermoedelijke onafhankelijke variabele
+data2= data$Smoke #Vermoedelijke afhankelijke variabele
+
+alpha=0.05
+
+# ---------------------------------------------- Berekeningen ----------------------------------------------
+model = chisq.test(data1,data2)
+model
+model$observed
+model$statistic
+p = model$p.value
+
+
+
+# p-waarde < α => H0 verwerpen (bij chi-kwadraat is dat altijd: er is geen verband.) Er is dus duidelijk een verband.
+
+l = model$parameter
+
+g = qchisq(1-alpha,df=l)
+
+chisq = model$statistic
+
+print(paste("Chi-kwadraat:     ", chisq))
+print(paste("g-waarde:         ", g))
+print(paste("p-waarde:         ", p))
+
+if(chisq > g){
+  print('H0 verwerpen! Er bestaat WEL een samenhang tussen de variabelen in de populatie!')
+  print(paste('chi-kwadraat (',round(chisq,3),') > g-waarde (',round(g,3),')'))
+  print(paste('p-waarde (',round(p,3),') < alpha (',alpha,')'))
+}else{
+  print('H0 niet verwerpen! Er bestaat GEEN samenhang tussen de variabelen in de populatie!')
+  print(paste('chi-kwadraat (',round(chisq,3),') < g-waarde (',round(g,3),')'))
+  print(paste('p-waarde (',round(p,3),') > alpha (',alpha,')'))
+  
+}
+
+# =========================================== Mozaik plot ======================================================
+plot(table(data1,data2), main="Excersise ~ Smoking", xlab = "Excersise level", ylab = "Smoking frequency")
